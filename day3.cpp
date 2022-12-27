b34390c4a05f36ae02729e4448ff3d2b96a87e86
Michel Eter <micoror1@gmail.com>
2022-12-27 14:45:58 -0800

    day3.2

--- day3.cpp
+++ day3.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
 #include <fstream>
+#include <unordered_map>
+#include <set>
 
 using namespace std;
+
+char FindCommon(string s1, string s2, string s3)
+{
+    unordered_map<char, int> counts;
+    set<char> set1(s1.begin(), s1.end()), set2(s2.begin(), s2.end()), set3(s3.begin(), s3.end());
+
+    for (char c : set1)
+    {
+        if (counts[c] < 1)
+            counts[c] += 1;
+    }
+    for (char c : set2)
+    {
+        if (counts[c] < 2)
+            counts[c] += 1;
+    }
+    for (char c : set3)
+    {
+        if (counts[c] < 3)
+            counts[c] += 1;
+        cout << c << counts[c] << endl;
+    }
+
+    for (auto i : counts)
+    {
+        if (i.second == 3)
+            return i.first;
+    }
+    return 0;
+}
 
 int GetPriority(char c)
 {
@@ -18,26 +50,16 @@ int main()
     ifstream HintFile;
     HintFile.open("./hints/HintFile3.txt");
 
-    string strLine;
+    string elf1, elf2, elf3;
     long priority = 0;
-    while (getline(HintFile, strLine))
+    while (HintFile >> elf1 >> elf2 >> elf3)
     {
-        char common;
-        int halfLength = strLine.length() / 2;
-        string compt1 = strLine.substr(0, halfLength);
-        string compt2 = strLine.substr(halfLength, 2 * halfLength);
-
-        for (int i = 0; i < compt2.size(); i++)
-        {
-            if (compt1.find(compt2[i]) != compt1.npos)
-            {
-                common = compt2[i];
-                cout << strLine.size() << " " << compt1 << " | " << compt2 << " " << common << endl;
-                break;
-            }
-        }
+        cout << elf1 << endl
+             << elf2 << endl
+             << elf3 << endl
+             << endl;
+        char common = FindCommon(elf1, elf2, elf3);
         priority += GetPriority(common);
     }
     cout << priority;
-    // cout << strLine << " " << strLine.size() << " " << compt1 << " | " << compt2 << endl;
 }
